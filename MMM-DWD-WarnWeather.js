4bca7bed5ad7d4f608d4d85d909a4f5f84315a39
heyheyhexi <tobger@aol.com>
2018-04-06 11:18:34 +0200

    Update MMM-DWD-WarnWeather.js

--- MMM-DWD-WarnWeather.js
+++ MMM-DWD-WarnWeather.js
@@ -73,7 +73,7 @@ Module.register("MMM-DWD-WarnWeather", {
 
 		var header = document.createElement("header");
 		if (this.community.hasOwnProperty('properties')){
-			header.innerHTML = 'TEST';
+			header.innerHTML = 'Wetterwarnungen';
 			if (this.config.displayRegionName && this.loaded) {
 				header.innerHTML += ' für:<br>' + this.community.properties.NAME;
 			}
